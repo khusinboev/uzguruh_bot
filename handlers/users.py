d70213fc2d0f52dc85db9859834181e86b17ea0f
Adkhambek <114991635+khusinboev@users.noreply.github.com>
2025-04-19 23:20:22 +0500

    Update users.py

--- handlers/users.py
+++ handlers/users.py
@@ -1,25 +1,27 @@
-from aiogram import Router, F
-from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram import Router
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import CommandStart
 
 user_router = Router()
 
 @user_router.message(CommandStart())
 async def start_handler(message: Message):
+    text = (
+        "🇺🇿 <i>Guruhga qo'shish uchun pastdagi\n"
+        "«➕ GURUHGA QO'SHISH ☑️» tugmasini bosing va guruhingizda administratorlik huquqini bering. Ko'proq ma'lumot...</i>\n"
+        "🇷🇺 <i>Чтобы добавить в группу, нажмите кнопку\n"
+        "«➕ GURUHGA QO'SHISH ☑️» и дайте права администратора. Больше информации...</i>\n"
+        "🇬🇧 <i>To add to the group, press the button\n"
+        "«➕ ADD TO GROUP ☑️» and give administrator rights. More information ...</i>"
+    )
+
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [
             InlineKeyboardButton(
-                text="➕ Guruhga qo‘shish",
+                text="➕ GURUHGA QO‘SHISH ☑️",
                 url="https://t.me/uzguruh_bot?startgroup=start"
             )
         ]
     ])
-
-    text = (
-        "<b>Assalomu alaykum!</b>\n\n"
-        "Ushbu bot guruhingizda reklamalarni avtomatik aniqlab, tozalashga yordam beradi.\n"
-        "Iltimos, <b>botni guruhingizga qo‘shing</b> va unga admin huquqlarini bering.\n\n"
-        "Botni guruhga qo‘shish uchun quyidagi tugmani bosing:"
-    )
 
     await message.answer(text, reply_markup=keyboard, parse_mode="HTML")
